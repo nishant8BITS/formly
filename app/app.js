f5d7677148baccc72d7200e912aa3fe50fe4fd0a
Nishant Kumar <nihantanu@gmail.com>
2015-05-22 19:56:00 +0530

    Re-structured the directory structure

---
+++ app/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name formlyApp
+ * @description
+ * # formlyApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('formlyApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
